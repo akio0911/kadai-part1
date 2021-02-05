1694e420f10d66a0a40601409443da4499a7003b
oonishireon <oonishireon110@gmail.com>
2021-02-05 14:27:14 +0900

    Initial Commit

---
+++ kadai-part1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kadai-part1
+//
+//  Created by 大西玲音 on 2021/02/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
